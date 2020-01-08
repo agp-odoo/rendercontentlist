6b5786c0e2c5dd7d71be570a538ef64bfc94f7aa
Ana-Gabriel <agp@odoo.com>
2020-01-07 16:46:12 -0800

    [REF] renderlistcontent: move all content into its own directory

--- renderlistcontent/__manifest__.py
+++ renderlistcontent/__manifest__.py
@@ -17,6 +17,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
+    'license': 'AGPL-3',
     'category': 'Render List Content',
     'version': '1.0',
 
