a2a0cb08b9b00672d98df1f10098b38a2d3112cf
Ana-Gabriel <agp@odoo.com>
2020-01-09 12:57:11 -0800

    [ADD] renderlistcontent: add manifest description

--- renderlistcontent/__manifest__.py
+++ renderlistcontent/__manifest__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Render List Content",
-    'description': "A widget that renders all content.",
-    'summary': """
+    'description': """
         This module holds a relational widget called 'render_list_content'. 
         It renders content with URLs accordingly: 
             - Webpages are rendered with the content listed in their meta tags.
@@ -10,6 +9,7 @@
             - Videos are rendered with video tags.
         The URL must start with an 'http://' or 'https://' for it to be valid.
     """,
+    'summary': "A widget that renders all content.",
 
     'author': "Odoo",
     'website': "https://www.odoo.com",
