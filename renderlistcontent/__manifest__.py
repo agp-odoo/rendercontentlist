68231075383458288ca3b170bd193920d44af7cf
Ana-Gabriel <agp@odoo.com>
2020-01-10 10:49:37 -0800

    [ADD] renderlistcontent: add proper category tag to manifest

--- renderlistcontent/__manifest__.py
+++ renderlistcontent/__manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'license': 'AGPL-3',
-    'category': 'Render List Content',
+    'category': 'Extra Tools',
     'version': '1.0',
 
     # any module necessary for this one to work correctly
